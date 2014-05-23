44fd027bb3269f46a1aba368aa86ae6867fdd8fc
Olblak <o.vernin@mondialtelecom.be>
2014-05-23 12:23:33 +0200

    Prepare ansible and add role zsh/vim

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
+
 ansible-lab
 ===========
 
-ansible-lab
+Required:
+    yum install ansible
+
+To configure vim in /etc:
+    ansible-playbook playbooks/configureLocalhost.yml
---
+++ plugins/lookup_plugins/dns.py
@@ -0,0 +1,23 @@
+import socket
+
+from ansible import utils, errors
+
+class LookupModule (object):
+    def __init__(self, basedir=None, **kwargs):
+        self.basedir = basedir
+
+    def run(self, terms, inject=None, **kwargs):
+        terms = utils.listify_lookup_plugin_terms(terms, self.basedir, inject)
+        if isinstance(terms, basestring):
+            terms = [terms]
+        ret = []
+        for term in terms:
+            try:
+                ret.append(socket.gethostbyname(term))
+            except socket.gaierror:
+                ret.append('0.0.0.0')
+            except socket.error, ex:
+                raise errors.AnsibleError("exception resolving %r" % (term,),
+                                          ex)
+        return ret
+
